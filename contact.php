00a2dc5d34d10a547ad66334de8ddd497a97ab2a
Molusoj <molusoj@gmail.com>
2020-07-13 15:48:51 +0100

    something

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['contact-name'];
+    $subject = $_POST['contact-subject'];
+    $emailFrom = $_POST['contact-email'];
+    $message = $_POST['contact-message'];
+
+
+    $mailTo = "Olusoji200@gmail.com";
+    $headers = "From:".$emailFrom;
+    $txt = "You have received an email from ".$name.".\n\n".$message;
+
+
+
+    mail($mailTo, $subject,  $txt, $headers);
+
+    header("Location:index.html");
+
+}
+
+?>
